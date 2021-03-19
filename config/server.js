fc7ad73756ea04d2cc2af08388b0960745feca9c
Prabin Poudel <probnpoudel@gmail.com>
2021-03-19 10:48:45 +0545

    Main/ Strapi backend setup for gatsby blog

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a098b983c7caccd0a98fc81db0946419'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cba38b5f-8b08-405e-9afe-8693afb2b335'
+};
